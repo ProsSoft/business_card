34bf4166768199cd88ce882deabedc09d28a4be0
Taras <ytaras@gmail.com>
2017-11-25 15:28:54 +0200

    test

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,26 +3,35 @@ import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { nav } from '../constants';
 
-const Navigation = ({ type }) => (
-  <nav className={`nav nav__${type}`}>
-    {type === 'aside' && (
-      <NavLink className={`nav__${type}-link`} to="/">
-        Home
-      </NavLink>
-    )}
-    {nav.map(elem => (
-      <NavLink
-        className={`nav__${type}-link`}
-        exact
-        activeClassName={`nav__${type}-link--active`}
-        key={elem.title}
-        to={elem.path}
-      >
-        {elem.title}
-      </NavLink>
-    ))}
-  </nav>
-);
+const Navigation = ({ type }) => {
+  const hashCheck = (match) => {
+
+    if (!match) {
+      return false;
+    }
+    return true;
+  };
+  return (
+    <nav className={`nav nav__${type}`}>
+      {type === 'aside' && (
+        <NavLink className={`nav__${type}-link`} to="/" activeClassName={`nav__${type}-link--active`}>
+          Home
+        </NavLink>
+      )}
+      {nav.map(elem => (
+        <NavLink
+          className={`nav__${type}-link`}
+          activeClassName={`nav__${type}-link--active`}
+          isActive={hashCheck}
+          key={elem.title}
+          to={elem.path}
+        >
+          {elem.title}
+        </NavLink>
+      ))}
+    </nav>
+  );
+};
 
 Navigation.propTypes = {
   type: PropTypes.string.isRequired
