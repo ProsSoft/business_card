0b3add6503ae73b99c39b9792117e07c38bdc038
Ruslan Vasylenko <ruslan.vasylenko87@gmail.com>
2019-04-07 11:49:54 +0300

    handling animated process-tree

--- src/components/Service/index.js
+++ src/components/Service/index.js
@@ -12,12 +12,12 @@ const { hero } = service;
 class Service extends Component {
   state = {
     src: 0,
-    timestamp: 0
+    deltaY: 0
   };
   render() {
     const { match: { params: { type }} } = this.props;
     const { title } = services.find(({link}) => link === `/${type}`);
-    const { src, timestamp } = this.state;
+    const { src, timestamp, deltaY: prevDeltaY } = this.state;
 
     return (
       <div className="service">
@@ -34,15 +34,17 @@ class Service extends Component {
             const y = window.scrollY;
             window.onscroll = () => window.scrollTo(x, y);
             const newTimestamp = performance.now();
-            this.setState({ timestamp: newTimestamp });
-            // if (newTimestamp - timestamp > 100) {
-            console.log(deltaY);
-              if (deltaY > 0 && src ) {
+            this.setState({ timestamp: newTimestamp, deltaY: deltaY + prevDeltaY });
+            if (newTimestamp - timestamp > 170) {
+              console.log('works', prevDeltaY);
+              const isDeltaYDecreased = this.state.deltaY < 0;
+              if (!isDeltaYDecreased && src) {
                 this.setState({ src: src - 1 })
-              } else if (deltaY < 0 && src < 6) {
+              } else if (isDeltaYDecreased && src < 6) {
                 this.setState({ src: src + 1 })
               }
-            // }
+              this.setState({ deltaY: 0 });
+            }
           }}
           onMouseOut={() => {
             window.onscroll = () => {};
