bef75475af13ba2f282c52cfbf3164c232ea2cb7
lybomyr1985 <zdrilyiliubomyr@gmail.com>
2019-10-17 09:12:35 +0300

    add some details

--- src/components/Careers/index.js
+++ src/components/Careers/index.js
@@ -1,10 +1,9 @@
 import React, { PureComponent } from 'react';
 import FaChevronCircleDown from 'react-icons/lib/fa/chevron-circle-down';
 import FaChevronCircleUp from 'react-icons/lib/fa/chevron-circle-up';
-import Check from 'react-icons/lib/fa/check';
-import Plus from 'react-icons/lib/fa/plus';
+import OfferContent from './offerContent'
 import Hero from '../common/Hero';
-import { careers , vacancy} from '../../constants';
+import { careers } from '../../constants';
 import { scrollTo } from "../../util";
 
 const { hero, offers } = careers;
@@ -33,7 +32,6 @@ export default class VacancyGroupButton extends PureComponent {
   render() {
     const scrollToElemId = 'header';
     return (
-      // eslint-disable-next-line react/jsx-filename-extension
       <div className="page careers">
         <Hero
           {...{
@@ -80,49 +78,3 @@ export default class VacancyGroupButton extends PureComponent {
       </div>);
   }
 };
-
-const OfferContent = ({ isOpen,
-                        isVacancyOpen,
-                        agenda,
-                        requirements,
-                        stack,
-                        mission,
-                        offering,
-                        conclusion }) => (
-  <div>
-      {!isVacancyOpen? <div className="closed_vacancy_badge">{ vacancy.closed}</div> : null}
-      <div className={`description ${isOpen ? 'opened' : ''  }`}>
-        {agenda}
-        <div className="flex-box">
-          <p className="title">{offering.title}</p>
-          <ul>
-            {offering.list.map(item =>
-              <li key={item}>
-                <Check />
-                {item}
-              </li>
-            )}
-          </ul>
-        </div>
-        <div className="flex-box">
-          <p className="title">{requirements.title}</p>
-          <ul>
-            {requirements.list.map((item, i, list) =>
-              <li key={item}>
-                {list.length - 1 === i ? <Plus /> : <Check />}
-                {item}
-              </li>
-            )}
-          </ul>
-        </div>
-        <p className="title">{stack.title}</p>
-        <p>{stack.text}</p>
-
-        <p className="title">{mission.title}</p>
-        <p>{mission.text}</p>
-
-        <p className="title">{conclusion.title}</p>
-        <p>{conclusion.text}</p>
-      </div>
-  </div>
-);
